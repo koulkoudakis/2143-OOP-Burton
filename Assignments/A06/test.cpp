6918b02e1b582ae71047f37f1b609bc0922de8bf
koulkoudakis <koulkoudakis@gmail.com>
2021-01-29 13:57:22 -0600

    A06

---
+++ Assignments/A06/test.cpp
@@ -0,0 +1,26 @@
+#include <SFML\Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(400, 400), "Sharome Burton");
+    sf::CircleShape shape(100.f);
+
+    shape.setFillColor(sf::Color::Green);
+
+    shape.move(sf::Vector2f(100,100));
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+
+    return 0;
+}
